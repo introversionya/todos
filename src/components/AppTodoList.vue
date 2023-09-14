c0e3518d3d61a1138410579c9331fcfb412492e6
Anton Demidenko <yaintroversivnost@gmail.com>
2023-09-14 13:02:43 +0500

    Добавление логики удаления задачи

--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -5,6 +5,7 @@
       :key="todo.id"
       :todo="todo"
       @toggle-todo="toggleTodo"
+      @remove-todo="removeTodo"
     />
   </ul>
 </template>
@@ -38,6 +39,9 @@ export default defineComponent({
       if (targetTodo) {
         targetTodo.completed = !targetTodo.completed
       }
+    },
+    removeTodo(id: number) {
+      this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
     }
   }
 });
